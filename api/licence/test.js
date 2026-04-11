5e8a6af5d5eadcb59cda4ea2925f6464d0dde1e4
izco0-star <izco0-star@users.noreply.github.com>
2026-04-11 18:17:35 +0200

    add missing test and telegram token endpoints

---
+++ api/licence/test.js
@@ -0,0 +1,7 @@
+﻿const { setCors, handleOptions } = require('../../lib/auth');
+
+module.exports = async function handler(req, res) {
+  setCors(res);
+  if (req.method === 'OPTIONS') return handleOptions(res);
+  return res.status(200).send('ok');
+};
