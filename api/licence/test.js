6263815a36c08da3037c536c15068a6c71c3648b
izco0-star <inigobravo70@gmail.com>
2026-04-20 02:45:17 +0200

    Delete api/licence directory

--- api/licence/test.js
+++
@@ -1,7 +0,0 @@
-﻿const { setCors, handleOptions } = require('../../lib/auth');
-
-module.exports = async function handler(req, res) {
-  setCors(res);
-  if (req.method === 'OPTIONS') return handleOptions(res);
-  return res.status(200).send('ok');
-};
